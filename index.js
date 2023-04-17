16d467a1129f2633fac01d1595f2f2ec0cd522b8
Towhidur-Rahman-Abid <abidrahman085@gmail.com>
2023-04-17 16:38:46 +0600

    change

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const jsonServer = require("json-server"); // importing json-server library
 const server = jsonServer.create();
+const auth = require("json-server-auth");
+
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 9000; //  chose port from here like 8080, 3001
 
 server.use(middlewares);
+app.use(auth);
 server.use(router);
 
 // Set default middlewares (logger, static, cors, etc.)
